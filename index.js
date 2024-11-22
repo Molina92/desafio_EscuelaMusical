64a8c68a7df774531e7c2d6e1170bc6ea5c05b15
Daniel Molina <danielmolina9202@gmail.com>
2024-11-22 20:11:23 -0300

    Funcion de edición de canciones añadida

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ app.listen(PORT, () => {
 app.use(cors());
 app.use(express.json());
 
+//Ruta para devolver el index.html
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html")
 })
 
+//Ruta para devolver el archivo canciones.json, sino existe lo crea y si existe lo devuelve
 app.get("/canciones", (req, res) => {
     !fs.existsSync("./canciones.json") ? fs.writeFileSync("./canciones.json", "[]")
         : (() => {
@@ -24,6 +26,7 @@ app.get("/canciones", (req, res) => {
         })();
 });
 
+//Ruta para añadir una canción al archivo canciones.json 
 app.post("/canciones", (req, res) => {
     const nuevaCancion = req.body;
 
@@ -34,6 +37,18 @@ app.post("/canciones", (req, res) => {
     res.send("Canción agregada con exito");
 });
 
+//Ruta para editar una canción del archivo canciones.json
+app.put("/canciones/:id", (req, res) => {
+    const { id } = req.params;
+    const cancion = req.body;
+
+    const canciones = JSON.parse(fs.readFileSync("./canciones.json", "utf-8"));
+    let index = canciones.findIndex((c) => c.id == id);
+    canciones[index] = cancion;
+    fs.writeFileSync("./canciones.json", JSON.stringify(canciones, null, canciones.length));
+
+    res.send("Canción editada con exito");
+});
 
 
 
